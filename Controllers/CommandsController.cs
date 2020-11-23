466bb503561c6c60e1e92cbc4e83d0a3d9db9c67
Omar Farouk <omar__farouk@hotmail.com>
2020-11-23 17:57:56 +0200

    Add project files.

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
+using Commander.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
+        }
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<CommandReadDto> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem != null)
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            else
+                return NotFound();
+        }
+
+        //POST api/commands
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+            return Ok(commandReadDto);
+        }
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,37 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "HowTo1", Line = "Line1", Platform = "Platfrom1" },
+                new Command { Id = 0, HowTo = "HowTo2", Line = "Line2", Platform = "Platfrom2" },
+                new Command { Id = 0, HowTo = "HowTo3", Line = "Line3", Platform = "Platfrom3" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            return new Command { Id = 0, HowTo = "HowTo1", Line = "Line1", Platform = "Platfrom1" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
